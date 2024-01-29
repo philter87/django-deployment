95529217f0e86be13885af7bdf5cc98ecc746f3d
Philip Hjorth Christiansen <pch@mjolner.dk>
2024-01-29 21:13:21 +0100

    feat: app=pages is added

--- README.md
+++ README.md
@@ -3,7 +3,8 @@ Illustrating Github actions + render.com + fly.io
 
 
 # Commands
-```
+```bash
+cd src
 python manage.py runserver
 
 # Go to http://localhost:8000/
--- django_deployment/urls.py
+++ django_deployment/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import pages.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', pages.views.home)
 ]
